b42fe6e59dbda4e99aab0df9c3d83b6adaa5cca8
Filip Krása <fidasek009@gmail.com>
2026-02-21 08:57:40 +0000

    ci: fix encore commands

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,4 +4,7 @@ set -euo pipefail
 # Install all workspace dependencies
 bun install
 
+# Disable Encore stealing your data
+cd backend && encore telemetry disable
+
 echo "Sukces! Kontainer sudah siap digunakan."
